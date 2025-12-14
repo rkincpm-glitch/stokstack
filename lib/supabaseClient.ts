50fa3f38b2bc1fc1a075d6543ad775b67b867805
rkincpm-glitch <rkincpm@gmail.com>
2025-12-13 23:28:52 -0500

    company login

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,7 @@
-// lib/supabaseClient.ts
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createSupabaseBrowserClient() {
-  const url = process.env.https://smunkjdnsybwsqwdvgsp.supabase.co!;
-  const anon = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtdW5ramRuc3lid3Nxd2R2Z3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MzU0MDUsImV4cCI6MjA4MDIxMTQwNX0.Ljxbjhwu-OX8rYuNpXDQOF6yy77-5raqZB9j88wOX3Y;
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
   return createBrowserClient(url, anon);
 }
